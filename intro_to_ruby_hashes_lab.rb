422dd868e6317db4892cc7924cfe327e80c5fc3b
Nazmul Kabir <nazmul.kabir1216@gmail.com>
2020-05-12 20:54:19 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -2,8 +2,9 @@ def base_hash
   # Remember implicit return! We're returning this new Hash _without_ the
   # keyword "return." Nice and neat.
   my_new_hash = {
-    :rail
+    :railroads => {}
   }
+  my_new_hash
 end
 
 def monopoly_with_second_tier
